59f886f6dcad695c76a7657f50472889ca22d00b
CaseyraeStarfinder <caseyrae@secondgalaxy.com>
2015-07-29 23:21:13 -0400

    Remove old code
    Signed-off-by: CaseyraeStarfinder <caseyrae@secondgalaxy.com>

--- indra/config.bat
+++
@@ -1,4 +0,0 @@
-@echo
-set lib=%lib%
-set include=%include%
-python develop.py -G vc100 -tRelease configure -DLAA:BOOL=ON -DUSE_PRECOMPILED_HEADERS:BOOL=ON -DLL_TESTS=OFF -DPACKAGE:BOOL=OFF  2>&1 |c:\cygwin\bin\tee Config.log
